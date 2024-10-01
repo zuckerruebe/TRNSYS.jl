1354dcb1c90c027e81138fef8e4d13884b720cae
Damian Birchler <damian.birchler@ost.ch>
2024-10-01 16:51:18 +0200

    Add methods units are expected to implement.

---
+++ unit.jl
@@ -0,0 +1,9 @@
+"""
+computeOutputsAfter(unit, timestep::TimestepInH, inputs::Inputs)::Outputs
+"""
+function computeOutputsAfter end
+
+"""
+setInputs!(unit, inputs::Inputs)::Nothing
+"""
+function setInputs! end
